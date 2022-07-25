fa7aeab3404a3ed1b8c44795d5cbb70dfa029048
Dima Soroko <84499772+DimaSoroko@users.noreply.github.com>
2022-07-25 14:01:53 +0200

    comments updated

--- includes/pipex.h
+++ includes/pipex.h
@@ -6,19 +6,40 @@
 /*   By: dsoroko <dsoroko@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/21 10:52:14 by dsoroko           #+#    #+#             */
-/*   Updated: 2022/07/22 14:22:59 by dsoroko          ###   ########.fr       */
+/*   Updated: 2022/07/25 13:59:38 by dsoroko          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_H
 # define PIPEX_H
 
-# include <unistd.h> 
-# include <sys/wait.h>
-# include <stdlib.h>
-# include <fcntl.h>
+/*
+** perror
+*/
 # include <stdio.h> 
+/*
+** open
+*/
+# include <fcntl.h>
+/*
+** dup2, close, STDIN_FILENO, fork, access
+*/
+# include <unistd.h> 
+/*
+** free
+*/
+# include <stdlib.h>
+/*
+** waitpid
+*/
+# include <sys/wait.h>
+/*
+** pid_t
+*/
 # include <sys/types.h>
+/*
+** libft.h
+*/
 # include "../libft/libft.h"
 
 void	errors(char *error);
